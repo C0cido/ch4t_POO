213e2897386dfbc750b7c797ee88c60fdeffdf34
Fabricio Valdez <fabriciovaldezz41@gmail.com>
2022-09-20 06:17:49 -0300

    login register

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
 import Login from './pages/Login';
+import Register from './pages/Register';
+import Login from './pages/Register';
 
 export default function App() {
   return (
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
       </Routes>
   );
 }
---
+++ src/pages/Register.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Register() {
+  return (
+    <div> Hola 3! </div>
+  )
+}
